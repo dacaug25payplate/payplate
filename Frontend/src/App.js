c8cea800cddb8a1507d87f0a0f4f9778b732ea27
sujit wandre <sujitwandre01@gmail.com>
2026-01-22 11:15:51 +0530

    Login component UI

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterComponent from './components/RegisterComponent';
+import LoginComponent from './components/LoginComponent';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes> 
-        <Route path="/" element={<RegisterComponent />} />
+        <Route path="/register" element={<RegisterComponent />} />
+        <Route path="/login" element={<LoginComponent />} />
       </Routes>  
     </BrowserRouter>
   );
---
+++ Frontend/src/components/LoginComponent.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className="container">
+        <div className="row justify-content-center align-items-center min-vh-100">
+        <div className="col-md-4">
+          <div className="card shadow p-4">
+            <h3 className="text-center mb-4">Login</h3>
+
+            <form>
+              <div className="mb-3">
+                <label className="form-label">Username</label>
+                <input
+                  type="text"
+                  name="username"
+                  className="form-control"
+                  placeholder="Enter username"
+                />
+              </div>
+
+              <div className="mb-3">
+                <label className="form-label">Password</label>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  className="form-control"
+                  placeholder="Enter password"
+                />
+              </div>
+
+              <div className="form-check mb-3">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="showPassword"
+                  checked={showPassword}
+                  onChange={() => setShowPassword(!showPassword)}
+                />
+                <label className="form-check-label small" >
+                  Show Password
+                </label>
+              </div>
+
+              <button type="submit" className="btn btn-primary w-100">
+                Login
+              </button>
+              <br />
+                <div className="text-center mt-3">
+                <a href="" className="d-block mb-2">
+                    Forgot Password?
+                </a>
+
+                <span className="me-1">Don't have an account?</span>
+                <Link to="/register">Register here</Link>
+                </div>
+
+              
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
