d7174ae2b7fa4c3f964a4929ae021dd83aacf007
Anurag <anuraganilyadav842002@gmail.com>
2026-01-25 21:10:26 +0530

    ..

--- frontend/src/pages/user/UserDashboard.jsx
+++
@@ -1,9 +0,0 @@
-function UserDashboard() {
-  return (
-    <h1 className="text-center mt-5">
-      Welcome to dashboard User
-    </h1>
-  );
-}
-
-export default UserDashboard;
--- frontend/src/pages/waiter/WaiterDashboard.jsx
+++
@@ -1,9 +0,0 @@
-function WaiterDashboard() {
-  return (
-    <h1 className="text-center mt-5">
-      Welcome to dashboard Waiter
-    </h1>
-  );
-}
-
-export default WaiterDashboard;
